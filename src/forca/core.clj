72699f4f1e9629e6a24649ea10d9dbeeb1bd4167
Rodrigo Baptista <rmb.tito@gmail.com>
2017-02-12 22:21:28 -0200

    Aula 06 completa aqui.

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -4,21 +4,29 @@
 
 (def total-de-vidas 6)
 
-(defn perdeu [] (println "Você perdeu"))
-(defn ganhou [] (println "Você ganhou!"))
+(defn perdeu [] (print "Você perdeu"))
+(defn ganhou [] (print "Você ganhou!"))
 
 (defn letras-faltantes [palavra acertos]
     (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
-    (empty? (letras-faltantes palavra acertos))
-)
+    (empty? (letras-faltantes palavra acertos)))
 
 (defn le-letra! [] (read-line))
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
+(defn imprime-forca [vidas palavra acertos]
+    (println "Vidas " vidas)
+    (doseq [letra (seq palavra)]
+        (if (contains? acertos (str letra))
+            (print letra " ")
+            (print "_" " ")))
+    (println))
+
 (defn jogo [vidas palavra acertos]
+    (imprime-forca vidas palavra acertos)
     (cond
         (= vidas 0) (perdeu)
         (acertou-a-palavra-toda? palavra acertos) (ganhou)
